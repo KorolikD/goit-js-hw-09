25213be800b2541a8a25d2f0e4f03efea040ae7f
Dmytro Korolik <dimytry.korolik@gmail.com>
2022-08-15 05:24:16 +0300

    1st task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const CHANGE_COLOR_DELAY = 1000;
+let timeoutID = null;
+let isActive = false;
+
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+
+refs.buttonStart.addEventListener('click', onButtonStart);
+refs.buttonStop.addEventListener('click', onButtonStop);
+
+function changeBodyBcgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onButtonStart() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  refs.buttonStart.setAttribute('disabled', false);
+
+  timeoutID = setInterval(() => {
+    changeBodyBcgColor();
+  }, CHANGE_COLOR_DELAY);
+}
+
+function onButtonStop() {
+  isActive = false;
+  clearInterval(timeoutID);
+  refs.buttonStart.removeAttribute('disabled');
+}
